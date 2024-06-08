c02612d194fc2d628886980bd0af0c00ae560869
lobosouza <lobosouza.it@gmail.com>
2024-06-08 01:09:38 -0300

    update

---
+++ ModuloProductos/menu.py
@@ -0,0 +1,10 @@
+''' Cuando se ingrese a la opción "1" del menu principal
+se abre el menu de productos:'''
+
+def menu_productos():
+    print("1. Ver producto")
+    print("2. Crear producto")
+    print("3. Actualizar producto")
+    print("4. Eliminar producto")
+    menu = input("ingrese una opción: ")
+#invocar función según la opción elegida While True: If - Elif - break
