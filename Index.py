106af2bf5c2bda83759043f7d5171b88ff35b664
lobosouza <lobosouza.it@gmail.com>
2024-06-08 00:29:55 -0300

    modificado

--- Index.py
+++ Index.py
@@ -1,1 +1,13 @@
+from CRUD_Productos import *
 
+while True:
+
+    print ("1. Productos")
+    print ("2. Ventas")
+    print ("3. Proveedor")
+    print ("4. Clientes")
+
+    menu = int(input("Ingrese una opción: "))
+
+    if menu == 1:
+        menu_productos()
