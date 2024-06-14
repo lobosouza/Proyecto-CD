a5da2d95c54e21196c9fa190b433b31c8205960c
lobosouza <lobosouza.it@gmail.com>
2024-06-14 04:40:36 -0300

    se eliminó una fila de las tablas proveedores y productos

--- BD/consultas_sql.sql
+++ BD/consultas_sql.sql
@@ -1,3 +1,8 @@
 select * from productos;
 INSERT INTO productos 
 VALUES (null,'Chispas', 400.00, 280.00, 100, 'Galletas con chispas de chocolate');
+INSERT INTO proveedores 
+VALUES (null,'Distribuidora ABC	', 'Av. Siempre Viva 123', 'contacto@abcdistribu.com', 1234567890);
+select * from proveedores;
+DELETE FROM productos WHERE ID_Producto=2;
+DELETE FROM proveedores WHERE ID_Proveedor=2;
