3a7138c5e48e2d49d0c647b629eaa1a7e8e589f8
Cdireni1 <direnicarlos1@gmail.com>
2024-06-17 14:11:19 -0300

    Uniendo el proyecto

---
+++ CRUD_Python_Mysql/Clientes.py
@@ -0,0 +1,23 @@
+from Conexion import *
+
+class Market:
+    
+          
+    
+    def ingresarClientes(DNI,Apellido_Nombre,Mail,Telefono,Direccion):
+        
+        
+        try:
+         cone = conexion.ConexionBaseDeDatos()
+         cursor = cone.cursor()
+         sql ="insert into Clientes values(null,%s,%s,%s,%s,%s);"
+         #La variable valores tiene que ser una tupla
+         valores = (DNI,Apellido_Nombre,Mail,Telefono,Direccion)
+         cursor.execute(sql,valores)
+         cone.commit()
+         print(cursor.rowcount,"Registro Ingresado")
+         cone.close()
+            
+        except mysql.connector.Error as error:
+            print("Error al ingresar los datos {}".format(error))
+            
