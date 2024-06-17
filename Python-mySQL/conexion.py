b1dd47bc5d3727545b2b674ff7f5a4c4b9a52694
lobosouza <169008119+lobosouza@users.noreply.github.com>
2024-06-17 14:31:22 -0300

    Delete Python-mySQL directory
    Se generó otra conexion, quedando esta inutilizada

--- Python-mySQL/conexion.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-conexion= mysql.connector.connect(user='user1', password = '123456', 
-                                   host='localhost', 
-                                   database='mydb', 
-                                   port='3306')
-print(conexion)
-
-                        
